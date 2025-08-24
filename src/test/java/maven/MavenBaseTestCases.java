e5280b86d3c3486ea868e28b1c99f21bd0ac219b
Isha Gupta <ishagupta2719@gmail.com>
2025-08-24 16:28:44 +0530

    Add 1st time code

--- src/test/java/maven/MavenBaseTestCases.java
+++ src/test/java/maven/MavenBaseTestCases.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MavenBaseTestCases {
     public static void main(String[] args) throws InterruptedException {
+
+
         WebDriver driver=new ChromeDriver();
         driver.get("https://jqueryui.com/datepicker/");
         driver.manage().window().maximize();
